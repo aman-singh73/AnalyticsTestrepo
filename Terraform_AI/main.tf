51aeac674972b6b6d1854e74d091fd0d6e0a76dd
Aman kumar singh <134715373+aman-singh73@users.noreply.github.com>
2026-06-01 14:19:50 +0530

    removed unamanged

--- Terraform_AI/main.tf
+++ Terraform_AI/main.tf
@@ -113,17 +113,7 @@ module "express_backend_app" {
   tags            = var.tags
 }
 
-# Module: unmanagedtest2060 (azurerm_storage_account)
-module "unmanagedtest2060" {
-  source = "./modules/azure-storage-account"
 
-  name = "unmanagedtest2060"
-  location = "eastus"
-  resource_group_name = "project-rg-7d6bab"
-  account_tier = "Standard"
-  account_replication_type = "LRS"
-  account_kind = "StorageV2"
-}
 
 # ========================================
 # Phase: 5 Virtual Machine (For Testing)
